41f9ab03dd2109e15942c79a79e63ecea2dcfb7d
Marouche <116162435+Marouchee@users.noreply.github.com>
2023-06-05 15:56:39 +0900

    Create server.c

--- server.c
+++ server.c
@@ -10,7 +10,7 @@ void main(){
   memset(&ssa, 0, sizeof(ssa));
   ssa.sin_family = AF_INET;                         // 인터넷 소켓
   ssa.sin_addr.s_aadr = htonl(INADDR_ANY);
-  ssa.sin_port = htons(11234)                       // 포트번호 11234
+  ssa.sin_port = htons(11234);                      // 포트번호 11234
     
   ss = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
   bind(ss, (struct sockaddt *) &ssa, sizeof(ssa));  // 바인딩
@@ -19,6 +19,6 @@ void main(){
     listen(ss, 10);
     sa = accept(ss, 0, 0);
     send(sa, 'test', 5, 0);                         // 5바이트 'test' 전송
-    close(sa)
+    close(sa);
   }
 }
